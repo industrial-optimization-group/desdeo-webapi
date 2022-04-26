31cec0d6b21cc75320d6a3ac0b8bec8456ce0185
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2022-04-26 12:50:55 +0300

    Added a log entry model and POST resource to log important information during the experiment. The model and resource need to be tested.

---
+++ models/log_models.py
@@ -0,0 +1,24 @@
+from app import db
+from sqlalchemy.orm import validates
+
+log_entry_types = ["Intermediate solution", "Final solution", "Info"]
+
+
+class LogEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    entry_type = db.Column(db.String(100), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    data = db.Column(db.String(1000), nullable=True)
+    info = db.Colum(db.String(1000), nullable=False)
+
+    def validate_type(self, _, type_):
+        if type_ not in log_entry_types:
+            raise ValueError(
+                f"The given entry_type {type_} is not an acceptable type. Acceptable type are {log_entry_types}."
+            )
+
+        return type_
+
+    def __repr__(self):
+        return f"LogEntry(id: {self.id}, time: {self.timestamp}, data: {self.data}, info: {self.info})"
---
+++ resources/log_resources.py
@@ -0,0 +1,59 @@
+from app import db
+import datetime
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_restx import Resource, reqparse
+from models.log_models import LogEntry, log_entry_types
+from models.user_models import UserModel
+import simplejson as json
+
+log_entry_parser = reqparse.RequestParser()
+log_entry_parser.add_argument(
+    "entry_type",
+    type=str,
+    help=f"The type should be one of {log_entry_types}.",
+    required=True,
+)
+log_entry_parser.add_argument(
+    "data",
+    type=str,
+    help="Possible data associated with the log entry.",
+    required=False,
+)
+log_entry_parser.add_argument(
+    "info",
+    type=str,
+    help="Info associated with the log entry.",
+    required=True,
+)
+
+
+class LogEntryResource(Resource):
+    @jwt_required()
+    def post(self):
+        current_user = get_jwt_identity()
+        current_user_id = UserModel.query.filter_by(username=current_user).first().id
+
+        data = log_entry_parser.parse_args()
+
+        log_entry_type = data["entry_type"]
+        log_data = data["data"]
+        log_info = data["info"]
+
+        try:
+            # create a new log entry
+            log_entry = LogEntry(
+                user_id=current_user_id,
+                entry_type=log_entry_type,
+                data=log_data,
+                info=log_info,
+                timestamp=datetime.datetime.now(),
+            )
+            db.session.add(log_entry)
+            db.session.commit()
+        except Exception as e:
+            print(
+                f"DEBUG: Got an exception while creating a LogEntry POST request: {e}"
+            )
+            return {"message": "Could not add log entry."}, 500
+
+        return {"message": "Log entry added successfully."}, 201
