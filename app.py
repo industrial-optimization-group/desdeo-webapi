8b711e88ac964af908c508685e479566500f816d
juropo <140176002+juropo@users.noreply.github.com>
2024-01-24 17:22:05 +0200

    Update app.py
    joo

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ api = Api(app)
 db_user = os.environ.get("POSTGRES_USER")
 db_password = os.environ.get("POSTGRES_PASSWORD")
 db_host = os.environ.get("POSTGRES_HOST")
-db_port = 5432 # os.environ.get("POSTGRES_PORT")
+db_port = os.environ.get("POSTGRES_PORT")
 db_name = os.environ.get("POSTGRES_DB")
 
 ACCESS_EXPIRES = timedelta(hours=2)
